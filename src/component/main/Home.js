0137c5dc86a5d5fcdc88b5d62c1ae8fc3f08051f
Waruni Imalsha <WaruniImalsha@horizoncampus.edu.lk>
2021-12-18 11:47:13 +0530

    home.js

--- src/component/main/Home.js
+++ src/component/main/Home.js
@@ -1,11 +1,12 @@
 import './Home.css';
 import search from '../img/search-icon.png'
 import TextField from '@mui/material/TextField';
+
 const Home = () => {
 
     return(
-    <div>
-        <div className="rcorners1">
+    <div className='backcolor'>
+        <div className="rcorners1" >
             
             <TextField
             label="Location"
