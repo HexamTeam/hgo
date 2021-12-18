a6756dab29d3cf0d16cc4463487c67f831b70ab2
Waruni Imalsha <WaruniImalsha@horizoncampus.edu.lk>
2021-12-18 14:38:58 +0530

    header.js

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@
 import './App.css';
 import Home from './component/main/Home';
 import Footer from './component/footer/Footer';
+import Header from './component/Header';
 
 function App() {
   return (
     <div>
-      
+      <Header/>
       <Home/>
       <Footer/>
       
--- src/component/main/Home.js
+++ src/component/main/Home.js
@@ -1,13 +1,7 @@
 import './Home.css';
 import search from '../img/search-icon.png'
 import TextField from '@mui/material/TextField';
-import LanguageIcon from '@mui/icons-material/Language';
-import IconButton from '@material-ui/core/IconButton';
-import PersonOutlineIcon from '@mui/icons-material/PersonOutline';
-import ReorderIcon from '@mui/icons-material/Reorder';
-import MenuIcon from '@mui/icons-material/Menu';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+
 
 const Home = () => {
 
@@ -17,25 +11,7 @@ const Home = () => {
 
 
         <div>
-            <button>HotelGo</button>
-            <button>Become a hotelier</button>
             
-            
-            <IconButton color="inherit">
-            <LanguageIcon fontSize="large" />
-            </IconButton>
-           
-            <IconButton color="inherit">
-            <MenuIcon fontSize="large" />
-            <Menu> 
-            <MenuItem>rt</MenuItem>
-            </Menu>
-          
-            {/* <MenuIcon>bh</MenuIcon> */}
-            <PersonOutlineIcon fontSize="large" />
-
-            
-            </IconButton>
 
         </div>
         <div className="rcorners1" >
